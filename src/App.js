d5df63f1eabb2b71ac3c201314e50b4f5e477b8d
Wojciech Piątkowski <chmurson@gmail.com>
2021-01-14 17:34:22 +0100

    working oAuth flow; dispalying user groups; dev and prod environment build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import Amplify, { Auth, Hub } from 'aws-amplify';
 
-import awsconfig from './aws-exports';
+import { awsConfig } from './custom-aws-exports';
 import logo from './logo.svg';
 import './App.css';
 
-Amplify.configure(awsconfig)
+Amplify.configure(awsConfig)
+
+console.log(awsConfig);
 
 function App() {
     const [user, setUser] = useState();
+    const [userData, setUserData] = useState();
+    const [_, setCustomState] = useState();
 
     useEffect(() => {
         Hub.listen("auth", ({ payload: { event, data } }) => {
             switch (event) {
                 case "signIn":
-                    this.setState({ user: data });
+                    setUser(data);
                     break;
                 case "signOut":
-                    this.setState({ user: null });
+                    setUser(null);
                     break;
                 case "customOAuthState":
-                    this.setState({ customState: data });
+                    setCustomState(data);
                     break;
                 default:
                     console.error(`Unknown event: ${event}`)
@@ -28,20 +32,56 @@ function App() {
         });
 
         Auth.currentAuthenticatedUser()
-            .then(returnedUser => setUser({ user: returnedUser }))
+            .then((user) => setUser(user))
             .catch(() => console.log("Not signed in"));
     }, []);
 
+    useEffect(() => {
+        if (user) {
+            user.getUserData((err, result) => {
+                setUserData(result)
+            })
+        }
+    }, [user])
+
+    const userEmail = useMemo(() => {
+        if (!userData) {
+            return undefined;
+        }
+
+        const emailAttribute = userData.UserAttributes.find(attr => attr.Name === "email");
+
+        if (!emailAttribute) {
+            return undefined
+        }
+
+        return emailAttribute.Value;
+    }, [userData])
+
+    const userGroups = useMemo(() => {
+        if (!user) {
+            return [];
+        }
+
+        const payload = user.getSignInUserSession().getAccessToken().decodePayload()
+        return payload['cognito:groups'] || [];
+    }, [user])
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                {user && <button onClick={() => Auth.signOut()}>Sign Out {user.getUsername()}</button>}
+                {user && (
+                    <div>
+                        <button onClick={() => Auth.signOut()}>Sign Out {user.getUsername()} {userEmail}</button>
+                        <p>User groups: {userGroups.length > 0 ? userGroups.join(', ') : <i>no groups</i>}</p>
+                    </div>
+                )}
                 {!user && <div>
                     <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>Open Facebook</button>
                     <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>Open Google</button>
-                    <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+                    <button onClick={() => Auth.federatedSignIn({ provider: 'COGNITO' })}>Open Hosted UI
+                    </button>
 
                 </div>}
             </header>
---
+++ src/custom-aws-exports.js
@@ -0,0 +1,11 @@
+import awsConfig from './aws-exports';
+
+const applicationHost = process.env.REACT_APP_APPLICAITON_URL || 'http://localhost:3000';
+
+awsConfig.oauth = {
+    ...awsConfig.oauth,
+    redirectSignIn: `${applicationHost}/token`,
+    redirectSignOut: `${applicationHost}/sign-out`
+};
+
+export { awsConfig }
